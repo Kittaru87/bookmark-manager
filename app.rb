5a04c206b43c4507e433ea2193532694f56d3070
Sophia Bell <Kittaru87@gmail.com>
2020-03-13 09:29:05 +0000

    adding in authentication method

--- app.rb
+++ app.rb
@@ -30,6 +30,16 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect('/bookmarks')
+  end
+
   get '/bookmarks' do
     @user = User.find(id: session[:user_id])
     @bookmarks = Bookmark.all
--- lib/user.rb
+++ lib/user.rb
@@ -4,6 +4,7 @@ require_relative 'database_connection'
 require 'bcrypt'
 
 class User
+  
   def self.create(email:, password:)
     encrypted_password = BCrypt::Password.create(password)
     result = DatabaseConnection.query("INSERT INTO users (email, password) VALUES ('#{email}', '#{encrypted_password}') RETURNING id, email;")
@@ -11,6 +12,14 @@ class User
       id: result[0]['id'],
       email: result[0]['email']
     )
+  end
+
+  def self.authenticate(email:, password:)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}';")
+    User.new(
+      id: result[0]['id'],
+      email: result[0]['email']
+      )
   end
 
   def self.find(id:)
